5b1822e9cf4d845051ec1bc9d7994ccff07789a9
barjenbruchlaw <brian@kcpropertylaw.com>
2022-07-12 11:40:31 -0500

    Work on breaking down and indexing docx elements

--- docx-dissector.py
+++ docx-dissector.py
@@ -1,13 +1,17 @@
 from docx import Document
 
-document = Document("message-template.docx")
+document = Document("Judgment - Jackson County.docx")
 
 paragraphs = document.paragraphs
 
-new_line = "Name: Fred"
+document_diagram = list()
 
-paragraphs[0].clear()
-    
-paragraphs[0].add_run(text=new_line)
-
-document.save('new-message.docx')
+for paragraph_count, paragraph in enumerate(paragraphs):
+    document_diagram.append([paragraph_count, list()])
+    document_diagram_paragraph_entry = document_diagram[paragraph_count][1]
+    runs = paragraph.runs
+    for run_count, run in enumerate(runs):
+        document_diagram_paragraph_entry.append([run_count, dict()])
+        document_diagram_run_entry = document_diagram_paragraph_entry[run_count][1]
+        document_diagram_run_entry.update({'text': run.text})
+print(document_diagram[2])
