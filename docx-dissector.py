18989c8e9f0e6ef0fe92fa98c58cf7d6b62a5fe9
barjenbruchlaw <brian@kcpropertylaw.com>
2022-07-13 11:28:08 -0500

    Document scan with all dict added

--- docx-dissector.py
+++ docx-dissector.py
@@ -1,14 +1,23 @@
 from docx import Document
 
-document = Document("Judgment - Jackson County.docx")
+document = Document("Test document.docx")
 
 paragraphs = document.paragraphs
 
 document_diagram = list()
+document_diagram.append(list())
+document_diagram_style = document_diagram[0]
+document_styles = document.styles
+document_styles_list = list()
+
+for style_item in document_styles:
+    document_styles_list.append(style_item)
+
+document_diagram_style.append(document_styles_list)
 
 for paragraph_count, paragraph in enumerate(paragraphs):
     document_diagram.append([paragraph_count, list()])
-    document_diagram_paragraph_entry = document_diagram[paragraph_count][1]
+    document_diagram_paragraph_entry = document_diagram[paragraph_count+1][1]
     document_diagram_paragraph_entry.append(dict())
     document_diagram_paragraph_format_elements = document_diagram_paragraph_entry[0]
     document_diagram_paragraph_format_elements.update({'alignment': paragraph.paragraph_format.alignment})
@@ -28,7 +37,17 @@ for paragraph_count, paragraph in enumerate(paragraphs):
         document_diagram_run_entry.update({'bold': run.bold})
         document_diagram_run_entry.update({'italic': run.italic})
         document_diagram_run_entry.update({'underline': run.underline})
+        document_diagram_run_entry.update({'subscript': run.font.subscript})
+        document_diagram_run_entry.update({'superscript': run.font.superscript})
+        document_diagram_run_entry.update({'highlight color': run.font.highlight_color})
+        document_diagram_run_entry.update({'strike through': run.font.strike})
+        document_diagram_run_entry.update({'double strike through': run.font.double_strike})
+        document_diagram_run_entry.update({'emboss': run.font.emboss})
+        document_diagram_run_entry.update({'imprint': run.font.imprint})
+        document_diagram_run_entry.update({'outline': run.font.outline})
+        document_diagram_run_entry.update({'shadow': run.font.shadow})
         document_diagram_run_entry.update({'font name': run.font.name})
         document_diagram_run_entry.update({'font size': run.font.size})
 
-print(document_diagram[2])
+
+print(document_diagram)
